203e7e6565e5ae329d0e797a6d61c14dbea73b20
Scott Griffiths <dr.scottgriffiths@gmail.com>
2024-07-14 15:52:27 +0100

    Simplification to parse_name_length_token

--- bitformat/_utils.py
+++ bitformat/_utils.py
@@ -10,11 +10,7 @@ NAME_INT_RE: Pattern[str] = re.compile(r'^([a-zA-Z][a-zA-Z0-9_]*?)(\d*)$')
 
 
 def parse_name_length_token(fmt: str) -> tuple[str, int | None]:
-    # Any single token with just a name and length
-    if m2 := NAME_INT_RE.match(fmt):
-        name = m2.group(1)
-        length_str = m2.group(2)
-        length = None if length_str == '' else int(length_str)
-        return name, length
-    raise ValueError(f"Can't parse 'name[length]' token '{fmt}'.")
-
+    if not (match := NAME_INT_RE.match(fmt)):
+        raise ValueError(f"Can't parse 'name[length]' token '{fmt}'.")
+    name, length_str = match.groups()
+    return name, int(length_str) if length_str else None
