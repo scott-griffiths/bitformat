11dc575f6f4952a4b719c53391c38451e38183fd
Scott Griffiths <dr.scottgriffiths@gmail.com>
2025-03-29 08:13:44 +0000

    Renaming simple_value to single_value in the parser.

--- bitformat/_dtypes.py
+++ bitformat/_dtypes.py
@@ -60,7 +60,7 @@ class DtypeTransformer(Transformer):
     def dtype_tuple(self, items) -> DtypeTuple:
         return DtypeTuple.from_params(items)
 
-    def simple_value(self, items) -> str:
+    def single_value(self, items) -> str:
         assert len(items) == 1
         return str(items[0])
 
