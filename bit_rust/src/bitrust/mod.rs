ff50cd24e32a33b7fc924609d1f2ede11a0269c4
Scott Griffiths <dr.scottgriffiths@gmail.com>
2025-05-12 20:36:53 +0100

    Start of a module reorg. Just moving stuff first.

---
+++ bit_rust/src/bitrust/mod.rs
@@ -0,0 +1,3 @@
+mod bits;
+pub use bits::BitRust;
+pub use bits::MutableBitRust;
--- bit_rust/src/lib.rs
+++ bit_rust/src/lib.rs
@@ -1,9 +1,9 @@
-pub mod bits;
+pub mod bitrust;
 use pyo3::prelude::*;
 
 #[pymodule]
 fn bit_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<bits::BitRust>()?;
-    m.add_class::<bits::MutableBitRust>()?;
+    m.add_class::<bitrust::BitRust>()?;
+    m.add_class::<bitrust::MutableBitRust>()?;
     Ok(())
 }
